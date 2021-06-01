cec9ec979f5e60b3b495500658f6fdbc5532f9b9
Divyai2i <divya@ideas2it.com>
2021-06-01 11:04:59 +0530

    Updated changes

--- src/main/java/com/userModule/UserApplication.java
+++ src/main/java/com/userModule/UserApplication.java
@@ -2,20 +2,12 @@ package com.userModule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
 
 @SpringBootApplication
 @EnableConfigServer
 public class UserApplication {
 
-	/*@Override  
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
-	{  
-		return application.sources(UserApplication.class);  
-	}*/  
 	public static void main(String[] args)   
 	{  
 		SpringApplication.run(UserApplication.class, args);  
